48feaad4f3e9c4d61586d6df10d747d6ff618b07
Bahdan Siamionau <bahdan.siamionau@gmail.com>
2016-09-13 23:45:54 +0300

    Add info about second meetup

--- deploy.sh
+++ deploy.sh
@@ -2,21 +2,41 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-# Build the project.
-hugo -t blackburn
-
-# Go To Public folder
+echo -e "\033[0;32mGo To Public folder...\033[0m"
 cd public
 
-# Commit changes.
+echo -e "\033[0;32mUpdating static site content...\033[0m"
+git pull origin master
+
+echo -e "\033[0;32mReturning...\033[0m"
+cd ..
+
+echo -e "\033[0;32mBuild the project...\033[0m"
+hugo -t blackburn
+
+echo -e "\033[0;32mGo To Public folder...\033[0m"
+cd public
+
+echo -e "\033[0;32mAdd changes to index...\033[0m"
 git add .
+
+echo -e "\033[0;32mCommiting changes...\033[0m"
 git commit -m "rebuilding site `date`"
+
+echo -e "\033[0;32mCreating new branch...\033[0m"
 git branch work
+
+echo -e "\033[0;32mChecking out master...\033[0m"
 git checkout master
+
+echo -e "\033[0;32mMerging new changes...\033[0m"
 git merge work
 
-# Push source and build repos.
+echo -e "\033[0;32mRemoving branch...\033[0m"
+git branch -d work
+
+echo -e "\033[0;32mPush source and build repos...\033[0m"
 git push origin master
 
-# Come Back
+echo -e "\033[0;32mReturning...\033[0m"
 cd ..
