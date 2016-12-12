2def17f54a9dae1f6654922ebe9a798b05944a07
Ruslan Ibragimov <ruslan@ibragimov.by>
2016-12-12 19:03:19 +0300

    Add git lfs to travis

--- .git-lfs-install.sh
+++ .git-lfs-install.sh
@@ -3,7 +3,7 @@
 gitlfs="1.5.3"
 
 mkdir -p $HOME/bin
-wget "https://github.com/git-lfs/git-lfs/releases/download/v$(gitlfs)/git-lfs-linux-amd64-$(gitlfs).tar.gz"
-tar xvfz "git-lfs-linux-amd64-$(gitlfs).tar.gz"
-mv "git-lfs-$(gitlfs)/git-lfs" $HOME/bin/git-lfs
+wget "https://github.com/git-lfs/git-lfs/releases/download/v${gitlfs}/git-lfs-linux-amd64-${gitlfs}.tar.gz"
+tar xvfz "git-lfs-linux-amd64-${gitlfs}.tar.gz"
+mv "git-lfs-${gitlfs}/git-lfs" $HOME/bin/git-lfs
 export PATH=$PATH:$HOME/bin/
